f3eb16fd1092824fd097ae5d4b207cacfc471e34
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-18 13:40:59 -0300

    configurando google access e instalando realmDb

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,17 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
 import backgroundImg from '../../assets/background.png';
 
 import { Container, Title, Slogan } from './styles';
 import { Button } from '../../components/Button';
 
-export function SignIn(){
+import { WEB_CLIENT_ID, IOS_CLIENT_ID } from '@env';
+import { Alert } from 'react-native';
+
+
+
+GoogleSignin.configure({
+  scopes: ['email', 'profile'],
+  webClientId: WEB_CLIENT_ID,
+  iosClientId: IOS_CLIENT_ID,
+});
+
+export function SignIn() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);;
+
+  async function handleGoogleSignIn() {
+    try {
+      setIsAuthenticating(true);
+
+      const { idToken } = await GoogleSignin.signIn();
+
+      if(idToken){
+
+      } else {
+        Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google');
+        setIsAuthenticating(false);
+      }
+
+    } catch (error) {
+      console.log(error);
+      Alert.alert('Entrar', 'Não foi possível conectar-se a sua conta Google.');
+      setIsAuthenticating(false);
+    }
+  }
+
   return (
     <Container source={backgroundImg}>
       <Title>Ignite Fleet</Title>
       <Slogan>Gestão de uso de veículos</Slogan>
       <Button
         title='Entrar com Google'
+        isLoading={isAuthenticating}
+        onPress={handleGoogleSignIn}
       />
     </Container>
   );
