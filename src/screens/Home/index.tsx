c00d3dccd5da9a9b0d25430f62cdfa55ce039a66
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-18 14:49:43 -0300

    criando o header

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,11 @@
+import { HomeHeader } from '../../components/HomeHeader';
 import { Container } from './styles';
 import { Text } from 'react-native';
 
 export function Home() {
   return (
     <Container>
-      <Text>Home</Text>
+      <HomeHeader />
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -2,4 +2,5 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
+  background: ${({ theme }) => theme.COLORS.GRAY_800};
 `;
