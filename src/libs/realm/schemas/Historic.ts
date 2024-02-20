8e821fc24f2a79ef832810650e5e0be4cd542ec5
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-19 23:02:40 -0300

    método generate

--- src/libs/realm/schemas/Historic.ts
+++ src/libs/realm/schemas/Historic.ts
@@ -1,8 +1,31 @@
 import { Realm } from '@realm/react';
 
+type GenerateProps = {
+  user_id: string;
+  description: string;
+  license_plate: string;
+}
+
 export class Historic extends Realm.Object<Historic> {
-  static generate(){
+  _id!: string;
+  user_id!: string;
+  license_plate!: string;
+  description!: string;
+  status!: string;
+  created_at!: string;
+  updated_at!: string;
+
+  static generate({ user_id, description, license_plate }: GenerateProps) {
     //aqui é o método que utilizamos para definir o schema
+    return {
+      _id: new Realm.BSON.UUID(),
+      user_id, 
+      license_plate,
+      description,
+      status: 'departure',
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
   }
 
   static schema = {
@@ -12,7 +35,7 @@ export class Historic extends Realm.Object<Historic> {
     primaryKey: '_id',
 
     properties: {
-      _id: 'uuid', 
+      _id: 'uuid',
       user_id: {
         type: 'string',
         indexed: true,
