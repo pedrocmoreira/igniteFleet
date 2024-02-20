687423931a15e9ac6b7a6fae78eec2b40ee9a6ed
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-19 22:36:47 -0300

    adicionando tipagem das rotas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from '@react-navigation/native';
 import { HomeHeader } from '../../components/HomeHeader';
 import { Container } from './styles';
 import { Text } from 'react-native';
