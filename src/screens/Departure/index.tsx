fb6ea94517870153a48766033dae350cf86ca622
Pedro Moreira <pedrocmoreira97@gmail.com>
2024-02-20 20:43:24 -0300

    criando textAreaInput

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Container, Content } from './styles';
 import { Header } from '../../components/Header';
 import { LicensePlateInput } from '../../components/LicensePlateInput';
+import { TextAreaInput } from '../../components/TextAreaInput';
 
 export function Departure() {
   return (
@@ -13,7 +14,13 @@ export function Departure() {
           label='Placa do Veículo'
           placeholder='BRA1234'
         />
+
+        <TextAreaInput 
+          label='Finalidade'
+          placeholder='Vou utilizar o veículo para...'
+        />
       </Content>
+
     </Container>
   );
 }
